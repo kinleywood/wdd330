44c60c28ae00fcc0de044cfa169e6ceb63b7dca5
kinleywood <kinleywood@gmail.com>
2022-05-05 10:16:08 -0600

    week 3 notes

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 let ol = document.querySelector("ol");
-const links = [{url:"week1/week1.html", label:"Week 1"}, {url:"week2/week2.html", label:"Week 2"}];
+const links = [{url:"week1/week1.html", label:"Week 1"}, {url:"week2/week2.html", label:"Week 2"},{url:"week3/week3.html", label:"Week 3"}];
 
 links.forEach(makeOrderedList);
 
