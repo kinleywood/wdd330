da6f80c3ed4fca1c785c7a52a471314d89e28875
kinleywood <kinleywood@gmail.com>
2022-05-18 10:15:35 -0600

    Co-authored-by: Tauriel1202 <Tauriel1202@users.noreply.github.com> Co-authored-by: Karol Bracho <bykarol@users.noreply.github.com>

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 let ol = document.querySelector("ol");
-const links = [{url:"week1/week1.html", label:"Week 1"}, {url:"week2/week2.html", label:"Week 2"},{url:"week3/week3.html", label:"Week 3"},{url:"week4/week4.html", label:"Week 4"}];
+const links = [{url:"week1/week1.html", label:"Week 1"}, {url:"week2/week2.html", label:"Week 2"},{url:"week3/week3.html", label:"Week 3"},{url:"week4/week4.html", label:"Week 4"},{url:"week5/week5.html", label:"Week 5"}];
 
 links.forEach(makeOrderedList);
 
