f4df532c5f68f02b84eb267696fd953f83f19798
kinleywood <kinleywood@gmail.com>
2022-06-18 18:56:46 -0600

    week 9

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 let ol = document.querySelector("ol");
-const links = [{url:"week1/week1.html", label:"Week 1"}, {url:"week2/week2.html", label:"Week 2"},{url:"week3/week3.html", label:"Week 3"},{url:"week4/week4.html", label:"Week 4"},{url:"week5/week5.html", label:"Week 5"},{url:"challengeOne", label:"ToDo List"},{url:"week7/week7.html", label:"Week 7"}, {url:"week8/week8.html", label:"Week 8"}];
+const links = [{url:"week1/week1.html", label:"Week 1"}, {url:"week2/week2.html", label:"Week 2"},{url:"week3/week3.html", label:"Week 3"},{url:"week4/week4.html", label:"Week 4"},{url:"week5/week5.html", label:"Week 5"},{url:"challengeOne", label:"ToDo List"},{url:"week7/week7.html", label:"Week 7"}, {url:"week8/week8.html", label:"Week 8"}, {url:"week9/week9.html", label:"Week 9"}];
 
 links.forEach(makeOrderedList);
 
